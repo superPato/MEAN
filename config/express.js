f9f7d492ef5f63cee181e97cbdcc13cf6164a9f2
Cesar Acual <chechaacual@gmail.com>
2019-04-19 19:02:26 -0600

    Add dependencies and configuration environment

--- config/express.js
+++ config/express.js
@@ -1,7 +1,25 @@
 const express = require('express');
+const morgan = require('morgan');
+const compress = require('compression');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 module.exports = function () {
     const app = express();
+
+    if (process.env.NODE_ENV === 'development') {
+        app.use(morgan('dev'));
+    } else if (process.env.NODE_ENV === 'production') {
+        app.use(compress());
+    }
+
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+
+    app.use(bodyParser.json());
+
+    app.use(methodOverride());
 
     require('../app/routes/index.server.routes.js')(app);
 
