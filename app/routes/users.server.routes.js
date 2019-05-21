4ea213150e9e6ef467de97b930039442dc29a4bd
Cesar Acual <chechaacual@gmail.com>
2019-05-20 23:54:22 -0600

    Deleting an existing user document

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -7,7 +7,8 @@ module.exports = function (app) {
 
 	app.route('/users/:userId')
 		.get(users.read)
-		.put(users.update);
+		.put(users.update)
+		.delete(users.delete);
 
 	// you use the app.param() method, which defines a middleware to be 
 	// executed before any other middleware that uses that parameter.
