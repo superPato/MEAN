4809a0a5b2ba71ee774cd7adea80660d517fe511
Cesar Acual <chechaacual@gmail.com>
2019-05-22 11:37:05 -0600

    Optimizing queries using indexes

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -4,10 +4,14 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
 	firstName: String,
 	lastName: String,
-	email: String,
+	email: {
+		type: String,
+		index: true
+	},
 	username: {
 		type: String,
-		trim: true
+		trim: true,
+		unique: true
 	},
 	password: String,
 	created: {
@@ -41,6 +45,12 @@ const UserSchema = new Schema({
 
 UserSchema.virtual('fullName').get(function () {
 	return this.firstName + ' ' + this.lastName;
+}).
+set(function (fullName) {
+	const splitName = fullName.split(' ');
+
+	this.firstName = splitName[0] || '';
+	this.lastName = splitName[1] || '';
 });
 
 UserSchema.set('toJSON', { getters: true, virtuals: true });
