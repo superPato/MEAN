1fd11f905471b8957bfefb36d3c923a233b18896
Cesar Acual <chechaacual@gmail.com>
2019-05-22 10:48:26 -0600

    Custom modifiers

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -13,7 +13,36 @@ const UserSchema = new Schema({
 	created: {
 		type: Date,
 		default: Date.now
+	},
+	website: {
+		type: String,
+		get: function (url) {
+			if (!url) {
+				return url;
+			} else {
+				if (url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
+					url = 'http://' + url;
+				}
+				return url
+			}	
+		},
+		set: function (url) {
+			if (!url) {
+				return url;
+			} else {
+				if (url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
+					url = 'http://' + url;
+				}
+				return url;
+			}
+		}
 	}
 });
+
+// This will force Mongoose to include getters when converting the 
+// MongoDB document to a JSON representation and will allow the 
+// output of documents using res.json() in order to include the 
+// getter's behavior.
+UserSchema.set('toJSON', { getters: true });
 
 mongoose.model('User', UserSchema);
